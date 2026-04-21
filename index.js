025ae4afce8a0295fb35f8ca6127fc697e11ccc3
graphento <idc>
2026-04-21 18:13:01 +0500

    5 how many drinks

--- index.js
+++ index.js
@@ -21,10 +21,32 @@ document
   .getElementById("add-button")
   .addEventListener("click", (e) => makeForm());
 
+const lightboxMessage = document.getElementById("lightbox-message");
 const lightboxToggle = document.getElementById("lightbox-toggle");
+
+function declineDrinks(number) {
+  const n = Math.abs(number) % 100;
+  const n1 = n % 10;
+
+  let word;
+
+  if (n > 10 && n < 20) {
+    word = "напитков";
+  } else if (n1 > 1 && n1 < 5) {
+    word = "напитка";
+  } else if (n1 === 1) {
+    word = "напиток";
+  } else {
+    word = "напитков";
+  }
+
+  return `${number} ${word}`;
+}
 
 document.getElementById("submit-button").addEventListener("click", (e) => {
   e.preventDefault();
+  const count = document.getElementsByClassName("beverage").length;
+  lightboxMessage.innerText = `Вы заказали ${declineDrinks(count)}`;
   lightboxToggle.checked = true;
 });
 
